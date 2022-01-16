cd96deec86381518d6b48371f571faedd94d39f5
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2022-01-16 10:02:22 -0700

    Initial Commit

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+
+const Button = ({color, text}) => {
+    return <button style={{ backgroundColor: color}} className='btn'>{text}</button>
+}
+
+Button.defaultProps = {
+    color: 'steelBlue'
+}
+
+Button.propTypes ={
+    text:PropTypes.string,
+    color: PropTypes.string,
+}
+
+
+// left off on events in the walkthrough
+export default Button
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+
+const Header = ({title}) => {
+    return (
+        <header class = 'header'>
+            <h1 >{title}</h1>
+            <Button color='green' text='Add'/>
+            <Button color='Blue' text='Hello'/>
+            <Button color='purple' text='Hello'/>
+            
+        </header>
+    )
+}
+
+//style={{color: 'green', backgroundColor: 'blue'}}
+
+Header.defaultProps = {
+    title:'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+}
+
+export default Header
+
