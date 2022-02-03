b0da0df80b2b1014699511a03f93151b44144538
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2022-02-02 23:07:52 -0700

    Messing with shapes some more

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component }  from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header'
+import './index.css';
+import Header from './components/Header';
 
 function App() {
 
 const name = 'Sean'
 
   return (
-    <div className="App">
-      <Header />
+    //<div className="App">
+    //  <Header />
+    //  square
+    //  <p class="text">SQUARE TEXT</p>
+    //</div>
+    //<div class="square">
+    //  <p class="text">square text</p>
+    //</div>
+    <div class="circle">
+      <p class="text">circle text</p>
     </div>
+
   );
 }
 
