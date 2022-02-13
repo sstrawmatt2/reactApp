80c1eccc1fb72c98b3bb510dd53d972c2bed8355
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2022-02-13 10:54:53 -0700

    Working on output for buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,6 +4,7 @@ import React, { Component }  from 'react';
 
 const Button = ({color, text}) => {
     return <button style={{ backgroundColor: color}} className='btn'>{text}</button>
+    
 }
 
 Button.defaultProps = {
